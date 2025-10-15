6e9232164254208d1e39ed2392c3ee19ed33694d
Aaditya nath vishwakarma <aadityanathvishwakarma@gmail.com>
2025-10-15 13:27:27 +0530

    Add landing page for Bank of Student
    Introduces last.php as a landing page with associated CSS styling and image. The page features a header, bank logo, call to action, navigation button, and footer for the Bank of Student project.

---
+++ final 11/last.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>BANK OF STUDENT 2</title>
+    <link rel="stylesheet" href="last.css" />
+  </head>
+  <body>
+    <div class="header">
+      <div class="bank-logo-small"><img src="bank3.jpg" alt="" /></div>
+      <div class="bank-name"><h2>BANK OF STUDENT (BOS)</h2></div>
+    </div>
+      
+          <h1 text align="center">Join our Bank</h1>      
+
+
+    <div class="last"><img src="las.png" alt=""></div>
+
+    <div>
+            <a href="welcome.php" class="nav-button">Home</a>
+
+    </div>
+           
+    <div class="footer">© group 6</div>
+  </body>
+</html>
